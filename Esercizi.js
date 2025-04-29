dbd25c81af642211a65b4544325aa945fec96bac
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-04-29 13:04:29 +0200

    Add: snack 4

--- Esercizi.js
+++ Esercizi.js
@@ -53,6 +53,21 @@ console.log(eseguiOperazione(4, 4, differenza))
 console.log(eseguiOperazione(4, 4, moltiplicazione))
 console.log(eseguiOperazione(4, 4, divisione))
 
+//SNACK 4
+//Crea un generatore di funzioni creaTimer
+//Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+function creaTimer(){
+
+    setTimeout(() => {
+        console.log('Tempo scaduto')
+    }, 1000)
+}
+
+creaTimer()
+
+
+
 
 
 
