31896ea4e4431c25c02183cd38df2511ee10583a
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-04-29 13:00:39 +0200

    Add: snack 3

--- Esercizi.js
+++ Esercizi.js
@@ -37,6 +37,22 @@ const quadrato1 = numero => numero * numero
 
 console.log(quadrato1(5))
 
+//SNACK 3
+//Crea una funzione eseguiOperazione
+//Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+const somma = (a, b) => a + b
+const differenza = (a, b) => a - b
+const moltiplicazione = (a, b) => a * b
+const divisione = (a, b) => a / b
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b)
+
+console.log(eseguiOperazione(4, 4, somma))
+console.log(eseguiOperazione(4, 4, differenza))
+console.log(eseguiOperazione(4, 4, moltiplicazione))
+console.log(eseguiOperazione(4, 4, divisione))
+
 
 
 
